d2932fed9af08ca7aa78b9053f6fd3fff87a7172
maru2222 <maru2222@gmail.com>
2015-08-05 09:59:01 +0900

    プラグイン化

--- script/new.mark.js
+++ script/new.mark.js
@@ -1,26 +1,36 @@
-$(function(){
-  $('p').each(function() {
-
-    // ソースからアップロードした日付を取得
-    var upLoad = $(this).data('upload-date');
-
-    // ソースからアップロードした日付New Dateへ格納
-    var upLoadDate = new Date(upLoad);
-
-    // 本日を取得
-    var today = new Date();
-
-    // 経過日数を取得
-    var keika = Math.floor((today - upLoadDate) / (1000 * 24 * 60 * 60));
-    // console.log(keika);
-
-    // 30日経過した経過した場合classを削除
-    if (keika > 30) {
-      $(this).removeClass('red');
-    }
+;(function($){
+  
+  $.fn.newmark = function(options) {
     
-  });
-});
+    options = $.extend({
+      elapsedDays: 30,
+      className: 'new'
+    }, options);
+    
+    this.each(function() {
+
+      // ソースからアップロードした日付を取得
+      var upLoad = $(this).data('upload-date');
+
+      // ソースからアップロードした日付New Dateへ格納
+      var upLoadDate = new Date(upLoad);
+
+      // 本日を取得
+      var today = new Date();
+
+      // 経過日数を取得
+      var keika = Math.floor((today - upLoadDate) / (1000 * 24 * 60 * 60));
+      // console.log(keika);
+
+      // 経過日数とアップロード日数を比較してclassを削除
+      if (keika > options.elapsedDays) {
+        $(this).removeClass(options.className);
+      }
+
+    });
+    return this;
+  }
+})(jQuery);
 
 /*------------------------
 
